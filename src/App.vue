6d3f53401d913fae5dfaf1dddc318e4aef054dc8
ConorHawk <conornhawkins@gmail.com>
2018-09-19 08:38:53 +1200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app" class="font-sans leading-normal antialiased">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import '@/assets/css/main.css'
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,158 @@
+<template>
+  <div>
+    <div class="" style="background-color:black">
+      <div class="container mx-auto flex flex-wrap items-center py-12">
+        <div class="w-full md:w-1/3 p-4">
+          <img src="/static/img/logo-white.png" alt="Logo">
+        </div>
+        <div class="w-1/2 md:w-1/3">
+          <img src="/static/img/header-bg.jpg" alt="hero image">
+        </div>
+        <div class="w-full md:w-1/3">
+        <h1 class="text-primary font-black font-oswald leading-tight flex flex-col items-center md:items-start" style="font-size:50px;line-height: 55px;">
+          <span>Survey</span>
+          <span>Insight</span>
+          <span>Action</span>
+        </h1>
+        </div>
+      </div>
+    </div>
+    <div class="bg-primary">
+      <div class="container mx-auto text-white py-12 flex flex-wrap items-center">
+        <div class="w-full md:w-1/3 p-4">
+        <p class="font-black text-2xl mb-6">We understand the impact of Business Confidence and Consumer Confidence, but until now we’ve never measured the effect of Employee Confidence.</p>
+        <p class="font-black text-2xl">And that’s where the K3 Employee Confidence Pulse Survey comes in.</p>
+        </div>
+        <div class="w-full md:w-2/3 p-4">
+          <p class="mb-4 font-semibold">The K3 Employee Confidence Pulse Survey uses 8 specifically designed questions to provide a snapshot of how confident employees are about their organisation’s potential for current and future success, and the impact of that confidence on business performance. </p>
+          <p class="mb-4">It then creates benchmarked results to provide actionable insights for businesses to drive greater performance through enhanced employee confidence.</p>
+          <p class="mb-4">As part of the Pulse, organisations also receive a link to a single survey question, which they can use to survey customers about their experience with the organisation. This measures the relationship between employee confidence and customer satisfaction, by linking Net Promotor Score to the organisation’s K3 Employee Confidence Pulse score.</p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="container mx-auto py-12">
+        <h2 class="text-primary text-center text-3xl px-4">AN EMPLOYEE SURVEY DELIVERING ACTIONABLE INSIGHTS</h2>
+        <div class="flex flex-wrap text-center py-8">
+          <div class="w-full md:w-1/3 p-4 relative">
+            <img class="w-24" src="/static/img/flow-1.svg" alt="">
+            <h3 class="text-primary text-xl mt-2">STEP 1: MEASURE</h3>
+            <p>Deploy your employee and customer survey, branded to your company, with K3-supplied link.</p>
+            <div class="hidden md:block absolute pin-t pin-r pt-8">
+              <i class="fas fa-chevron-right text-3xl text-primary" style="transform:scaleY(1.3)"></i>
+            </div>
+            <div class="block md:hidden pt-8">
+              <i class="fas fa-chevron-down text-3xl text-primary" style="transform:scaleX(1.3)"></i>
+            </div>
+          </div>
+          <div class="w-full md:w-1/3 p-4 relative">
+            <img class="w-24" src="/static/img/flow-2.svg" alt="">
+            <h3 class="text-primary text-xl mt-2">STEP 2: INSIGHT</h3>
+            <p>Get real-time results and insights via your personalised dashboard.</p>
+            <div class="hidden md:block absolute pin-t pin-r pt-8">
+              <i class="fas fa-chevron-right text-3xl text-primary" style="transform:scaleY(1.3)"></i>
+            </div>
+            <div class="block md:hidden pt-8">
+              <i class="fas fa-chevron-down text-3xl text-primary" style="transform:scaleX(1.3)"></i>
+            </div>
+          </div>
+          <div class="w-full md:w-1/3 p-4">
+            <img class="w-24" src="/static/img/flow-3.svg" alt="">
+            <h3 class="text-primary text-xl mt-2">STEP 3: ACTION</h3>
+            <p>Take action based on the insights, with guidance from K3’s expert consultants.</p>
+          </div>          
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="container mx-auto py-12">
+        <h2 class="text-primary text-center text-3xl px-4">CHOOSE YOUR DEPTH OF INSIGHTS</h2>
+        <div class="flex flex-wrap">
+          <div class="px-6 py-2 w-full md:w-1/3">
+            <div class="border-primary border-2 p-4">
+              <h4 class="text-primary text-center text-xl mb-3">BASE INSIGHTS</h4>
+              <div class="flex mb-3">
+                <i class="fas fa-caret-right text-primary text-2xl mr-2"></i>
+                <p>Businesses receive their overall company Employee Confidence Index score, compared to the overall market. </p>
+              </div>
+               <div class="flex mb-3">
+                <i class="fas fa-caret-right text-primary text-2xl mr-2"></i>
+                <p>Receive their scores on each of the eight individual Index items compared to the market.</p>
+              </div>
+               <div class="flex mb-3">
+                <i class="fas fa-caret-right text-primary text-2xl mr-2"></i>
+                <p>Can survey their customers using a single question about their experience with the organisation, to gauge the relationship between employee confidence and customer satisfaction.</p>
+              </div>
+              <p class="text-center text-2xl font-bold text-primary mb-6 mt-4">FREE FOR 2018!</p>
+            </div>
+          </div>
+          <div class="px-6 py-2 w-full md:w-1/3">
+            <div class="p-4 bg-gold-dark border-2 border-gold-dark text-white h-full flex flex-col justify-between">
+              <div>
+                <h4 class="text-white text-center text-xl mb-3">BASE INSIGHTS</h4>
+                <p class="mb-3">Businesses receive Base Insights <span class="font-bold">PLUS:</span></p>
+                <div class="flex mb-3">
+                  <i class="fas fa-caret-right text-white text-2xl mr-2"></i>
+                  <p>Explore deeper against additional factors including position type, tenure and gender. </p>
+                </div>
+              </div>
+              <div class="my-4">
+                <p class="text-center text-xl font-bold text-white mb-2 mt-2">1-50 EMPLOYEES: $400</p>
+                <p class="text-center text-xl font-bold text-white mb-2 mt-2">51-150 EMPLOYEES: $600</p>
+                <p class="text-center text-xl font-bold text-white mb-2 mt-2">151+ EMPLOYEES: $900</p>
+              </div>
+            </div>
+          </div>
+          <div class="px-6 py-2 w-full md:w-1/3">
+            <div class="p-4 bg-grey-dark border-2 border-grey-dark text-white h-full flex flex-col justify-between">
+              <div>
+                <h4 class="text-white text-center text-xl mb-3">PLATINUM INSIGHTS</h4>
+                <p class="mb-3">Businesses receive Gold Insights  <span class="font-bold">PLUS:</span></p>
+                <div class="flex mb-3">
+                  <i class="fas fa-caret-right text-white text-2xl mr-2"></i>
+                  <p>Our experienced consultants will take this information to the deepest level of analysis, interpreting it against multiple factors, and providing tailored guidance as to where and how to increase these confidence levels.</p>
+                </div>
+              </div>
+              <div class="my-4">
+                <p class="text-center text-xl font-bold text-white mb-2 mt-2">1-50 EMPLOYEES: $800</p>
+                <p class="text-center text-xl font-bold text-white mb-2 mt-2">51-150 EMPLOYEES: $1,200</p>
+                <p class="text-center text-xl font-bold text-white mb-2 mt-2">151+ EMPLOYEES: 1,500</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="w-full border-primary border-t-4" style="background-color:black">
+      <div class="container mx-auto flex flex-wrap items-center">
+        <div class="w-full md:w-1/3 p-2">
+          <img style="width:250px" src="/static/img/footer-logo.svg" alt="">
+        </div>
+        <div class="w-2/3 md:w-1/3 p-2">
+        <div class="">
+          <a class="text-white no-underline font-bold bg-primary text-white px-4 py-1 block text-center" href="http://www.employeeconfidence.co.nz/">CLICK HERE TO REGISTER AND FOR MORE INFO</a>
+        </div>
+        </div>
+        <div class="w-1/3 md:w-1/3 p-2">
+          <p class="font-bold text-white text-right">09 366 1366</p>
+        </div>
+      </div>
+      
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
